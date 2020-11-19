a57c84e94be6f219eebb64f0def5c8be03763699
Davyd Tevzadze <pawtod20@gmail.com>
2020-11-19 22:30:15 +0200

    init commit, typography, styles, librarys, hero-section, second section.

---
+++ app/js/common.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+    //parallax
+
+    $('.section-bg').scrolly();
+
+    //share-dropdown
+
+    let shareButton = document.querySelector('.share-button')
+
+    $(shareButton).on('click', function() {
+        $(this).toggleClass('active');
+        $('.share-dropdown').toggleClass('active')
+    })
+
+
+    //imagesGrow
+
+    let groveImage = document.querySelectorAll('.image-grow');
+
+    function imagesGrow(images) {
+        for (let i = 0; i < images.length; i++) {
+
+            let imageHeight = images[i].getBoundingClientRect().height;
+            let docHeight = document.documentElement.clientHeight;
+            let offsetTop = images[i].getBoundingClientRect().y;
+            let imageBottom = docHeight - imageHeight - offsetTop
+            console.log(offsetTop, imageBottom)
+
+            if (offsetTop < docHeight - 400) {
+                images[i].classList.add('active')
+
+            } else {
+                images[i].classList.remove('active')
+            }
+            if (offsetTop + imageHeight < 0) {
+                console.log('hi')
+                images[i].classList.remove('active')
+            }
+        }
+    }
+
+    $(window).on('scroll', function() {
+        imagesGrow(groveImage);
+    })
+
+
+});
